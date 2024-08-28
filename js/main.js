7418630fa5d11265fa0d2700a86a672d121d90c5
Mohamed Amr <126739293+Mohamed-F22@users.noreply.github.com>
2024-08-28 13:18:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -186,6 +186,14 @@ data = {
             "title": "project 3",
             "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias nihil quidem a excepturi quasi commodi nostrum molestiae inventore itaque, veniam quam aperiam maxime minus corrupti aliquam incidunt praesentium vero sint. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet tenetur culpa neque eaque, ab obcaecati deleniti esse dolorum reiciendis iure soluta nam molestiae totam minima asperiores. Incidunt quos quam dolores.",
             "source": "https://www.youtube-nocookie.com/embed/YrRkju7sDKA?si=RdLMKKsPXV8pPBkg"
+        },
+        {
+            "name": "Mostafa Gamal",
+            "date": "Dec 28, 2023",
+            "location": "Egypt, Giza",
+            "title": "project 3",
+            "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias nihil quidem a excepturi quasi commodi nostrum molestiae inventore itaque, veniam quam aperiam maxime minus corrupti aliquam incidunt praesentium vero sint. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet tenetur culpa neque eaque, ab obcaecati deleniti esse dolorum reiciendis iure soluta nam molestiae totam minima asperiores. Incidunt quos quam dolores.",
+            "source": "https://www.youtube.com/embed/rAPJWmfmc1M"
         }
     ],
     "reelsSrcInfo" : [
@@ -207,112 +215,91 @@ data = {
     ]
 }
 // project present
-// fetch('../json/details.json')
-//     .then(function(response) {
-//         return response.json()
-//     })
-//     .then (function(projectsInformation) {
-        let projectsInfo = data.projectsInfo
+let projectsInfo = data.projectsInfo
+let project = document.querySelector(".port-project")
+let clientName = document.querySelector(".port-project .client-name")
+let projectDate = document.querySelector(".port-project .date")
+let projectLocation = document.querySelector(".port-project .location")
+let projectTitle = document.querySelector(".port-project h1")
+let dateLocation = document.querySelector(".port-project .date-location")
+let projectdesc = document.querySelector(".port-project .description")
+let video = document.querySelector(".port-project .video-content iframe")
+let branchVidLanding = document.querySelector(".branch-landing-vid")
+if (project !== null) {
+    let projectIndex = project.getAttribute("id") - 1
+    let clientNameTxt = projectsInfo[projectIndex].name
+    let projectDateTxt = projectsInfo[projectIndex].date
+    let projectLocationTxt = projectsInfo[projectIndex].location
+    let projectTitleTxt = projectsInfo[projectIndex].title
+    let dateLocationTxt = `${projectDateTxt} | ${projectLocationTxt}`
+    let projectdescTxt = projectsInfo[projectIndex].description
+    video.setAttribute("src", projectsInfo[projectIndex].source)
+    clientName.innerHTML = clientNameTxt
+    projectDate.innerHTML = projectDateTxt
+    projectLocation.innerHTML = projectLocationTxt
+    projectTitle.innerHTML = projectTitleTxt
+    dateLocation.innerHTML = dateLocationTxt
+    projectdesc.innerHTML = projectdescTxt
 
-        let project = document.querySelector(".port-project")
-        let clientName = document.querySelector(".port-project .client-name")
-        let projectDate = document.querySelector(".port-project .date")
-        let projectLocation = document.querySelector(".port-project .location")
-        let projectTitle = document.querySelector(".port-project h1")
-        let dateLocation = document.querySelector(".port-project .date-location")
-        let projectdesc = document.querySelector(".port-project .description")
-        let video = document.querySelector(".port-project .video-content iframe")
-        let branchVidLanding = document.querySelector(".branch-landing-vid")
-
-        if (project !== null) {
-            let projectIndex = project.getAttribute("id") - 1
-            let clientNameTxt = projectsInfo[projectIndex].name
-            let projectDateTxt = projectsInfo[projectIndex].date
-            let projectLocationTxt = projectsInfo[projectIndex].location
-            let projectTitleTxt = projectsInfo[projectIndex].title
-            let dateLocationTxt = `${projectDateTxt} | ${projectLocationTxt}`
-            let projectdescTxt = projectsInfo[projectIndex].description
-
-            video.setAttribute("src", projectsInfo[projectIndex].source)
-            clientName.innerHTML = clientNameTxt
-            projectDate.innerHTML = projectDateTxt
-            projectLocation.innerHTML = projectLocationTxt
-            projectTitle.innerHTML = projectTitleTxt
-            dateLocation.innerHTML = dateLocationTxt
-            projectdesc.innerHTML = projectdescTxt
-        
-            window.scrollTo({
-                top: branchVidLanding.offsetHeight,
-                left: 0,
-                behavior: 'smooth'
-            });
-        }
-    // })
+    window.scrollTo({
+        top: branchVidLanding.offsetHeight,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
 // reels slider
-// fetch("../json/details.json")
-//     .then(function (response) {
-//         return response.json()
-//     })
-//     .then(function (reelsSrcInformation) {
-        let reelsSrc = data.reelsSrcInfo
-        let leftIcon = document.querySelector(".reels .container .icon-left")
-        let rightIcon = document.querySelector(".reels .container .icon-right")
-        let reels = document.querySelector(".reels .reels-container .slider iframe")
-        
-        if (reels !== null) {
-            let i = 0
+let reelsSrc = data.reelsSrcInfo
+let leftIcon = document.querySelector(".reels .container .icon-left")
+let rightIcon = document.querySelector(".reels .container .icon-right")
+let reels = document.querySelector(".reels .reels-container .slider iframe")
+
+if (reels !== null) {
+    let i = 0
+    reels.setAttribute("src", `${reelsSrc[i]}`)
+    rightIcon.onclick = function () {
+        if (i === (reelsSrc.length - 1)) {
+            i = 0
             reels.setAttribute("src", `${reelsSrc[i]}`)
-            rightIcon.onclick = function () {
-                if (i === (reelsSrc.length - 1)) {
-                    i = 0
-                    reels.setAttribute("src", `${reelsSrc[i]}`)
-                }else {
-                    i = i + 1
-                    reels.setAttribute("src", `${reelsSrc[i]}`)
-                }
-            }
-            leftIcon.onclick = function () {
-                if (i === 0) {
-                    i = (reelsSrc.length - 1)
-                    reels.setAttribute("src", `${reelsSrc[i]}`)
-                }else {
-                    i = i - 1
-                    reels.setAttribute("src", `${reelsSrc[i]}`)
-                }
-            }
+        }else {
+            i = i + 1
+            reels.setAttribute("src", `${reelsSrc[i]}`)
         }
-    // })
+    }
+    leftIcon.onclick = function () {
+        if (i === 0) {
+            i = (reelsSrc.length - 1)
+            reels.setAttribute("src", `${reelsSrc[i]}`)
+        }else {
+            i = i - 1
+            reels.setAttribute("src", `${reelsSrc[i]}`)
+        }
+    }
+}
 // chroma slider
-// fetch("../json/details.json")
-//     .then(function (response) {
-//         return response.json()
-//     })
-//     .then(function (reelsChromaSrcInformation) {
-        let reelsChromaSrc = data.reelsChromaSrcInfo
-        let leftIconCh = document.querySelector(".reels-chroma .container .icon-left")
-        let rightIconCh = document.querySelector(".reels-chroma .container .icon-right")
-        let reelsCh = document.querySelector(".reels-chroma .reels-container .slider iframe")
-        
-        if (reelsCh !== null) {
-            let i = 0
+let reelsChromaSrc = data.reelsChromaSrcInfo
+let leftIconCh = document.querySelector(".reels-chroma .container .icon-left")
+let rightIconCh = document.querySelector(".reels-chroma .container .icon-right")
+let reelsCh = document.querySelector(".reels-chroma .reels-container .slider iframe")
+
+if (reelsCh !== null) {
+    let i = 0
+    reelsCh.setAttribute("src", `${reelsChromaSrc[i]}`)
+    rightIconCh.onclick = function () {
+        if (i === (reelsChromaSrc.length - 1)) {
+            i = 0
             reelsCh.setAttribute("src", `${reelsChromaSrc[i]}`)
-            rightIconCh.onclick = function () {
-                if (i === (reelsChromaSrc.length - 1)) {
-                    i = 0
-                    reelsCh.setAttribute("src", `${reelsChromaSrc[i]}`)
-                }else {
-                    i = i + 1
-                    reelsCh.setAttribute("src", `${reelsChromaSrc[i]}`)
-                }
-            }
-            leftIconCh.onclick = function () {
-                if (i === 0) {
-                    i = (reelsChromaSrc.length - 1)
-                    reelsCh.setAttribute("src", `${reelsChromaSrc[i]}`)
-                }else {
-                    i = i - 1
-                    reelsCh.setAttribute("src", `${reelsChromaSrc[i]}`)
-                }
-            }
+        }else {
+            i = i + 1
+            reelsCh.setAttribute("src", `${reelsChromaSrc[i]}`)
         }
-    // })
+    }
+    leftIconCh.onclick = function () {
+        if (i === 0) {
+            i = (reelsChromaSrc.length - 1)
+            reelsCh.setAttribute("src", `${reelsChromaSrc[i]}`)
+        }else {
+            i = i - 1
+            reelsCh.setAttribute("src", `${reelsChromaSrc[i]}`)
+        }
+    }
+}
